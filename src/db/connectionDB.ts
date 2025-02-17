99386a122ca433de2a459fed1162ca5084a227ba
Ilia <6967221@gmail.com>
2025-02-17 12:44:01 +0300

    get test data from mongo db

--- src/db/connectionDB.ts
+++ src/db/connectionDB.ts
@@ -1,24 +1,42 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri =
-    'mongodb+srv://6967221:yGssUyRhCra4EIUF@cluster-lesson3.b6dqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-lesson3';
+const MONGO_URI =
+  'mongodb+srv://6967221:yGssUyRhCra4EIUF@cluster-lesson3.b6dqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-lesson3';
 
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+const DB_NAME = 'sample_airbnb';
+const COLLECTIONS = {
+  listingsAndReviews: 'listingsAndReviews',
+} as const;
+
+const client = new MongoClient(MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
+export let collections: {
+  listingsAndReviews: any;
+} = {
+  listingsAndReviews: null,
+};
+
 export const runDb = async () => {
-    try {
-        await client.connect();
-        await client.db('admin').command({ ping: 1 });
-        console.log('Pinged your deployment. You successfully connected to MongoDB!');
-    } catch (error) {
-        console.error('Failed to connect to MongoDB:', error);
-        // await client.close();
-        process.exit(1);
-    }
+  try {
+    await client.connect();
+    const db = client.db(DB_NAME);
+
+    collections.listingsAndReviews = db.collection<any>(COLLECTIONS.listingsAndReviews);
+
+    await client.db('admin').command({ ping: 1 });
+
+    console.log('✅ Успешное подключение к MongoDB!');
+
+    const data = await collections.listingsAndReviews.find().limit(10).toArray();
+    console.log('📊 Данные из коллекции listingsAndReviews:', data);
+  } catch (error) {
+    console.error('❌ Ошибка подключения к MongoDB:', error);
+    process.exit(1);
+  }
 };
