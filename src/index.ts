36fb2ee99bb0cb50722fca46bf75dcb3d9e20ead
Ilia <6967221@gmail.com>
2025-02-17 10:30:43 +0300

    add 500

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Express } from 'express';
 import { blogsRouter } from './routes/blogs/router';
 import { testingRouter } from './routes/testing/router';
 import { postsRouter } from './routes/posts/router';
-import {logErr, runDb} from "./db/connectionDB";
+import { runDb } from './db/connectionDB';
 
 const app: Express = express.default();
 const port: number = Number(process.env.PORT) || 3000;
@@ -17,20 +17,8 @@ app.use('/', blogsRouter);
 app.use('/', testingRouter);
 app.use('/', postsRouter);
 
-const startApp = async () => {
-  try {
-    app.listen(port, () => {
-      console.log(`Сервер запущен на порту ${port}`);
-    });
-  } catch (error) {
-    console.error('Failed to start application:', error);
-    process.exit(1);
-  }
-};
-
-process.on('SIGINT', async () => {
-  process.exit();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-
-runDb()
+runDb();
