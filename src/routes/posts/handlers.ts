06baf4470edb495077b1d73e4add463a57bb42f6
Ilia <6967221@gmail.com>
2025-02-18 10:36:33 +0300

    Add find by id posts and blogs

--- src/routes/posts/handlers.ts
+++ src/routes/posts/handlers.ts
@@ -17,21 +17,29 @@ export const getPosts = async (_req: Request, res: Response) => {
   }
 };
 
-export const getPostById = (req: any, res: any) => {
-  const post = posts.find(p => p.id === req.params.id);
+export const getPostById = async (req: any, res: any) => {
+  try {
+    const post = await collections.posts?.findOne(
+      { id: req.params.id },
+      { projection: { _id: 0 } },
+    );
 
-  if (!post) {
-    return res.status(404).json({
-      errorsMessages: [
-        {
-          message: 'Post not found',
-          field: 'id',
-        },
-      ],
-    });
+    if (!post) {
+      return res.status(404).json({
+        errorsMessages: [
+          {
+            message: 'Post not found',
+            field: 'id',
+          },
+        ],
+      });
+    }
+
+    res.status(200).json(post);
+  } catch (error) {
+    console.error('❌ Ошибка при получении поста:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
-
-  res.status(200).json(post);
 };
 
 export const createPost = (req: any, res: any) => {
