744bdfcd1e1c70b57f90844e3e98a145855504f7
Ilia <6967221@gmail.com>
2025-02-18 22:31:14 +0300

    fix delete method

--- src/routes/testing/router.ts
+++ src/routes/testing/router.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import { collections } from '../../db/connectionDB';
 
 export const testingRouter = Router();
 
+testingRouter.delete('/testing/all-data', async (_req, res) => {
+  try {
+    await collections.blogs?.deleteMany({});
+    await collections.posts?.deleteMany({});
 
-//TODO: чистить на mongo
-testingRouter.delete('/testing/all-data', (_req, res) => {
-  const blogs = require('../../mocks/blogs.mock').blogsData;
-  blogs.length = 0;
-  res.sendStatus(204);
+    res.sendStatus(204);
+  } catch (error) {
+    console.error('❌ Ошибка при очистке данных:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
